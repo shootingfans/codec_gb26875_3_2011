e0ac0f87c6f94c046136a4cd0d941b14486e363d
Jarod <xinghen_tj@qq.com>
2021-08-15 02:05:28 +0800

    add ReaderDecoder and coverage testing for ReaderDecoder

---
+++ codec/reader_decoder.go
@@ -0,0 +1,70 @@
+package codec
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"runtime"
+	"sync/atomic"
+	"time"
+
+	"github.com/shootingfans/codec_gb26875_3_2011/constant"
+)
+
+type ReaderDecoder struct {
+	*readerDecoder
+}
+
+type readerDecoder struct {
+	reader       *bufio.Reader
+	close        chan struct{}
+	closed       uint32
+	C            chan *constant.Packet
+	readDuration time.Duration
+}
+
+func (r *readerDecoder) Close() error {
+	if atomic.CompareAndSwapUint32(&r.closed, 0, 1) {
+		close(r.close)
+	}
+	return nil
+}
+
+func (r *readerDecoder) cron() {
+	defer close(r.C)
+	for {
+		select {
+		case <-r.close:
+			return
+		default:
+			b, _ := r.reader.Peek(256)
+			p, n, err := Decode(b)
+			if err != nil {
+				if errors.Is(err, ErrPacketInvalid) || errors.Is(err, ErrPacketChecksumInvalid) {
+					r.reader.Discard(n)
+				}
+				continue
+			}
+			select {
+			case <-r.close:
+				return
+			case r.C <- p:
+			}
+		}
+	}
+}
+
+func NewReaderDecoder(reader io.Reader) *ReaderDecoder {
+	rc := &ReaderDecoder{
+		&readerDecoder{
+			reader: bufio.NewReader(reader),
+			close:  make(chan struct{}),
+			C:      make(chan *constant.Packet),
+		},
+	}
+	go rc.cron()
+	runtime.SetFinalizer(rc, func(r *ReaderDecoder) {
+		_ = r.Close()
+	})
+	return rc
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/stretchr/testify v1.7.0
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0
 	golang.org/x/text v0.3.6
 )
